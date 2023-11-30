e4bf71b2d762c77c45b886ab7431e2fe3f42110d
Joel Perez <joel_perez65@yahoo.com>
2023-11-29 23:22:02 -0600

    added bar chart and styles

--- src/app/components/LandingPage/LandingPage.tsx
+++ src/app/components/LandingPage/LandingPage.tsx
@@ -1,11 +1,25 @@
 "use client";
+import BarChart from "../BarChart/BarChart";
 import LineChart from "../LineChart/LineChart";
 
 export default function LandingPage() {
   return (
-    <div className="h-full w-3/4">
-      <h2 className="text-white text-3xl mb-6">Overview</h2>
-      <LineChart />
+    <div className="h-full w-1/2 p-4" style={{ background: "rgb(30, 30, 40)" }}>
+      <h2 className="text-white text-3xl mb-6 ml-12">Your Overview</h2>
+      <div className="flex justify-around">
+        <div
+          className="rounded-2xl p-4 w-full md:w-2/5 min-w-fit h-full text-white"
+          style={{ background: "rgb(20, 20, 30)" }}
+        >
+          <LineChart />
+        </div>
+        <div
+          className="rounded-2xl p-4 w-full md:w-2/5 min-w-fit h-full text-white"
+          style={{ background: "rgb(20, 20, 30)" }}
+        >
+          <BarChart />
+        </div>
+      </div>
     </div>
   );
 }
